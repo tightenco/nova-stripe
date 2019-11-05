a1ad74ad107733ea90f19b7f568af4a9cbedc8e9
Jesper Jacobsen <joj@webshipper.com>
2019-11-05 22:29:23 +0100

    Allow currency filtering

--- src/Http/StripeBalanceController.php
+++ src/Http/StripeBalanceController.php
@@ -9,6 +9,17 @@ class StripeBalanceController extends Controller
 {
     public function index()
     {
-        return response()->json(['balance' => (new StripeClient)->getBalance()]);
+        $balance = (new StripeClient)->getBalance();
+        $values = ['available', 'pending'];
+
+        if (count(config('nova-stripe.currencies', []))) {
+            foreach ($values as $value) {
+                $balance->{$value} = collect($balance->{$value})->filter(function ($x) {
+                    return in_array($x->__debugInfo()['currency'], config('nova-stripe.currencies'));
+                })->toArray();
+            }
+        }
+
+        return response()->json(['balance' => $balance]);
     }
 }
